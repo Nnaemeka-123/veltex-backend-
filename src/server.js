5039ef237aa76cdaabd092957aafa3d962a08aec
Nnaemeka-123 <wazobiaking47@gmail.com>
2026-08-07 23:44:57 +0100

    Remove unused video and feed routes

--- src/server.js
+++ src/server.js
@@ -3,9 +3,7 @@ const express = require('express');
 const cors = require('cors');
 
 const authRoutes = require('./routes/auth');
-const videoRoutes = require('./routes/videos');
 const socialRoutes = require('./routes/social');
-const feedRoutes = require('./routes/feed');
 
 const app = express();
 app.use(cors());
@@ -14,9 +12,7 @@ app.use(express.json());
 app.get('/health', (req, res) => res.json({ ok: true }));
 
 app.use('/auth', authRoutes);
-app.use('/videos', videoRoutes);
-app.use('/feed', feedRoutes);
-app.use('/', socialRoutes); // exposes /videos/:id/like, /users/:id/follow, etc.
+app.use('/', socialRoutes);
 
 app.use((err, req, res, next) => {
   console.error(err);
